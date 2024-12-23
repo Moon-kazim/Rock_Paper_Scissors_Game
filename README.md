10d16ed6a0667b0565990c8d575584b569a475d4
MOON-KAZIM <kazim.shaikh0@gmail.com>
2024-12-23 10:19:26 +0530

    updated code of game

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rock_Paper_Scissors_Game
 
-You are going to build a Rock, Paper, Scissors game. You will need to use what you have learnt about randomisation and Lists to achieve this.
+We built a Rock, Paper, Scissors game. 
--- Rock_Paper_Scissors_Game_Generator.py
+++ Rock_Paper_Scissors_Game_Generator.py
@@ -1,3 +1,5 @@
+print("Welcome To Rock, Paper and Scissors game")
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,65 +26,131 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-print("Welcome To Rock, Paper and Scissors game")
-import random
-# image = [rock, paper, scissors]
-random_numb = random.randint(0,2)
-# 0 == rock
-# 1 == paper
-# 2 == scissors
-choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+#game images
+game_images = [rock, paper, scissors]
 
-# print(f"Computer Choice: {random_numb}")
+#input from the user
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:\n"))
+print("")
+
+#indexing of image
+if choice >= 0 and choice <=2:
+    print("YOU choice")
+    print(game_images[choice])
+    
+    #input from the computer
+    random_numb = random.randint(0,2)
+    print("Computer Choice:")
+    print(game_images[random_numb])
+
+#invalid entery    
+if choice >=3 or choice<0:
+    print("invalid number enter,\nYOU LOST!!")
 
 #stone
-if choice == 0 and random_numb == 2:
-    print("Your Choice: rock")
-    print(rock)
-    print("Computer Choice: scissors")
-    print(scissors)
+elif choice == 0 and random_numb == 2:
     print("You Win!!")
 elif choice == 0 and random_numb == 1:
-    print("Your Choice: rock")
-    print(rock)
-    print("Computer Choice: paper")
-    print(paper)
     print("You Lost!!")
 
 #paper
 elif choice == 1 and random_numb == 0:
-    print("Your Choice: paper")
-    print(paper)
-    print("Computer Choice: rock")
-    print(rock)
     print("You Win!!")
 elif choice == 1 and random_numb == 2:
-    print("Your Choice: paper")
-    print(paper)
-    print("Computer Choice: scissors")
-    print(scissors)
     print("You Lost!!")
 
 #scissors
 elif choice == 2 and random_numb == 1:
-    print("Your Choice: scissors")
-    print(scissors)
-    print("Computer Choice: paper")
-    print(paper)
     print("You Win!!")
 elif choice == 2 and random_numb == 0:
-    print("Your Choice: scissors")
-    print(scissors)
-    print("Computer Choice: stone")
-    print(rock)
     print("You Lost!!")
 
 #Draw
 elif choice == random_numb:
-    print(f"Your Choice: {choice}")
-    print(f"Computer Choice: {random_numb}")
     print("DRAW!!")
 
-#invalid entery    
-else:
-    print("invalid number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #input from the computer
+# random_numb = random.randint(0,2)
+# # rock == 0
+# # paper == 1
+# # scissors == 2
+
+# #input from the user
+# choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+
+# # print(f"Computer Choice: {random_numb}")
+
+
+# #invalid entery    
+# if choice >=3 or choice<0:
+#     print("invalid number enter,\nYOU LOST!!")
+
+# #stone
+# elif choice == 0 and random_numb == 2:
+#     print("Your Choice: rock")
+#     print(rock)
+#     print("Computer Choice: scissors")
+#     print(scissors)
+#     print("You Win!!")
+# elif choice == 0 and random_numb == 1:
+#     print("Your Choice: rock")
+#     print(rock)
+#     print("Computer Choice: paper")
+#     print(paper)
+#     print("You Lost!!")
+
+# #paper
+# elif choice == 1 and random_numb == 0:
+#     print("Your Choice: paper")
+#     print(paper)
+#     print("Computer Choice: rock")
+#     print(rock)
+#     print("You Win!!")
+# elif choice == 1 and random_numb == 2:
+#     print("Your Choice: paper")
+#     print(paper)
+#     print("Computer Choice: scissors")
+#     print(scissors)
+#     print("You Lost!!")
+
+# #scissors
+# elif choice == 2 and random_numb == 1:
+#     print("Your Choice: scissors")
+#     print(scissors)
+#     print("Computer Choice: paper")
+#     print(paper)
+#     print("You Win!!")
+# elif choice == 2 and random_numb == 0:
+#     print("Your Choice: scissors")
+#     print(scissors)
+#     print("Computer Choice: stone")
+#     print(rock)
+#     print("You Lost!!")
+
+# #Draw
+# elif choice == random_numb:
+#     print(f"Your Choice: {choice}")
+#     print(f"Computer Choice: {random_numb}")
+#     print("DRAW!!")
+
+# #invalid entery    
+# # else:
+# #     print("invalid number")
